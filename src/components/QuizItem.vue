36d64a0f2b7482c44db6c8a311ec87d6f4d21641
Ed <ed.jones@gmail.com>
2023-12-14 07:12:53 -0500

    Replaces entire quizItem

--- src/components/QuizItem.vue
+++ src/components/QuizItem.vue
@@ -2,7 +2,7 @@
   <div class="w-full place-content-center  mx-auto">
     <div class="quiz-item ">
       <h5 class="text-stone-400">{{ quizItem.title }}</h5>
-      <p class="question-text">{{ quizItems.Question }}</p>
+      <p class="question-text">{{ quizItem.Question }}</p>
       <ul class="">
         <li class="flex flex-row p-4 bg-stone-400 answer">
           <div class="list-asking"><svg xmlns="http://www.w3.org/2000/svg" height="25" width="25"
@@ -11,7 +11,7 @@
                 d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
             </svg>
           </div>
-          <div class="list-item-right">{{ quizItems[0].option1 }}
+          <div class="list-item-right">{{ quizItem.option1 }}
           </div>
         </li>
         <li class="flex flex-row p-4 bg-stone-400 border-solid border-amber-500 rounded-lg answer ">
@@ -20,7 +20,7 @@
               <path opacity="1" fill="#1E3050"
                 d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
             </svg></div>
-          <div class=""> {{ quizItems[0].option2 }}</div>
+          <div class=""> {{ quizItem.option2 }}</div>
         </li>
         <li class="flex flex-row p-4 answer">
           <div class="list-asking"><svg xmlns="http://www.w3.org/2000/svg" height="25" width="25"
@@ -28,7 +28,7 @@
               <path opacity="1" fill="#1E3050"
                 d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
             </svg></div>
-          <div class="list-item-right">{{ quizItems[0].option3 }}</div>
+          <div class="list-item-right">{{ quizItem.option3 }}</div>
         </li>
         <li class="flex flex-row p-4 answer">
           <div class="list-asking"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16"
@@ -36,68 +36,17 @@
               <path opacity="1" fill="#1E3050"
                 d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
             </svg></div>
-          <div class="list-item-right">{{ quizItems[0].option4 }}</div>
+          <div class="list-item-right">{{ quizItem.option4 }}</div>
         </li>
       </ul>
       <div class="w-2/3 mx-auto">
-        <p>{{ quizItems[0].explanation }} <a href="https://youtu.be/Nh3r2WZs2P8?si=9wZ9XWrCTioUaVkG">Phoneme Addition</a>
+        <p>{{ quizItem.explanation }} <a href="https://youtu.be/Nh3r2WZs2P8?si=9wZ9XWrCTioUaVkG">Phoneme Addition</a>
         </p>
         <div class="flex flex-row"><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"
             viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc. -->
             <path opacity="1" fill="#FFBF00"
               d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" />
-          </svg> <span class="caution-message">{{ quizItems[0].caution }}</span></div>
-      </div>
-    </div>
-  </div>
-
-
-  <div class="border-slate-200  w-full md:w-1/2 lg:w-2/3 px-4 place-content-center  mx-auto">
-    <div class="border-stone-300 w-72 quiz-item ">
-      <h5 class="text-stone-400">{{ quizItems[1].title }}</h5>
-      <p class="question-text">{{ quizItems[1].Question }}</p>
-      <ul class="">
-        <li class="w-full p-4 bg-stone-400 answer">
-          <span class="list-asking"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16"
-              viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc. -->
-              <path opacity="1" fill="#cc3333"
-                d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
-            </svg></span>
-          <span class="list-item-right">{{ quizItems[1].option1 }} </span>
-        </li>
-        <li class="w-full p-4 rounded-lg answer">
-          <span class="list-asking"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16"
-              viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc. -->
-              <path opacity="1" fill="#1E3050"
-                d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
-            </svg></span>
-          <span class="list-item-right"> {{ quizItems[1].option2 }}</span>
-        </li>
-        <li class="w-full p-4 answer">
-          <span class="list-asking"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16"
-              viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc. -->
-              <path opacity="1" fill="#1E3050"
-                d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
-            </svg></span>
-          <span class="list-item-right">{{ quizItems[1].option3 }}</span>
-        </li>
-        <li class="w-full p-4 answer">
-          <span class="list-asking"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16"
-              viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc. -->
-              <path opacity="1" fill="#1E3050"
-                d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
-            </svg></span>
-          <span class="list-item-right">{{ quizItems[1].option4 }}</span>
-        </li>
-      </ul>
-      <div class="place-content-center text-center explanation">
-        <p class="text-center">{{ quizItems[1].explanation }}
-        </p>
-        <div><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"
-            viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc. -->
-            <path opacity="1" fill="#FFBF00" stroke="#FFBF00;"
-              d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" />
-          </svg> {{ quizItems[1].caution }}</div>
+          </svg> <span class="caution-message">{{ quizItem.caution }}</span></div>
       </div>
     </div>
   </div>
