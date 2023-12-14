14246ed662473564435fa7eadcf98bf974ebfdf9
Ed <ed.jones@gmail.com>
2023-12-14 08:52:18 -0500

    Adding color

--- src/components/QuizItem.vue
+++ src/components/QuizItem.vue
@@ -26,7 +26,7 @@
         <li class="flex flex-row p-4 answer" @click="optionSelected(3)">
           <div class="list-asking"><svg xmlns="http://www.w3.org/2000/svg" height="25" width="25"
               viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc. -->
-              <path opacity="1" fill="#1E3050"
+              <path opacity="1" fill="#ff0000" stroke="#00ffff"
                 d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
             </svg></div>
           <div class="list-item-right">{{ quizItem.option3 }}</div>
@@ -45,7 +45,7 @@
         </p>
         <div class="flex flex-row mt-8"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16"
             viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc. -->
-            <path opacity="1" :fill="iconColor" :stroke="iconColor"
+            <path opacity="1" fill="#FFBF00"
               d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" />
           </svg> <span class="caution-message">{{ quizItem.caution }}</span></div>
       </div>
