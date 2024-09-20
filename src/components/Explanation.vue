93a14396efde307b661d2a14a1ef70d4a8c8784b
Ed <ed.jones@gmail.com>
2024-09-20 05:51:32 -0400

    Video looks great

--- src/components/Explanation.vue
+++ src/components/Explanation.vue
@@ -3,23 +3,24 @@
         <!-- Display explanation content here -->
         <div :class="{ [`hidden`]: !reviewMode }"
             class="border-yellow-500 rounded-md explanation p-4 lg:mt-4 sm:mt-2 place-self-center lg:ml-20 lg:w-5/6">
-            <p class="text-amber-700 mb-4">{{ quizItem.explanation }}
-                <!--a href="https://youtu.be/Nh3r2WZs2P8?si=9wZ9XWrCTioUaVkG">Phoneme Addition</a-->
-            </p>
-            <p class="text-amber-700">{{ quizItem.explanation2 }}
-            </p>
-            <p class="text-amber-700">{{ quizItem.explanation3 }}
-            </p>
-            <p v-if="quizItem.ref1 != ''" class="text-amber-700 mt-2 text-sm">{{ quizItem.ref1 }}
-            </p>
-            <p v-if="quizItem.ref2 != ''" class="text-amber-700 text-sm">{{ quizItem.ref2 }}
-            </p>
-            <p v-if="quizItem.ref3 != ''" class="text-amber-700 mb- text-sm">{{ quizItem.ref3 }}
-            </p>
-            <iframe v-if="quizItem.videoUrl" width="560" height="315" :src="quizItem.videoUrl"
-                title="YouTube video player" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="content-wrapper">
+                <div class="text-content">
+                    <!-- Move all the text content here -->
+                    <p class="text-amber-700 mb-4">{{ quizItem.explanation }}</p>
+                    <p class="text-amber-700">{{ quizItem.explanation2 }}</p>
+                    <p class="text-amber-700">{{ quizItem.explanation3 }}</p>
+                    <p v-if="quizItem.ref1 != ''" class="text-amber-700 mt-2 text-sm">{{ quizItem.ref1 }}</p>
+                    <p v-if="quizItem.ref2 != ''" class="text-amber-700 text-sm">{{ quizItem.ref2 }}</p>
+                    <p v-if="quizItem.ref3 != ''" class="text-amber-700 mb- text-sm">{{ quizItem.ref3 }}</p>
+                </div>
+                <div v-if="quizItem.videoUrl" class="video-wrapper">
+                    <div class="video-container">
+                        <iframe :src="quizItem.videoUrl" title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
             <!----
       <p><a
           href="https://www.researchgate.net/profile/Matthew-Burns-10/publication/321116132_Meta-analysis_of_targeted_small-group_reading_interventions/links/5ab162340f7e9b4897c39acd/Meta-analysis-of-targeted-small-group-reading-interventions.pdf">{{
@@ -59,5 +60,47 @@ export default {
     padding: 1rem;
     border: 1px solid #ccc;
     border-radius: 4px;
+}
+
+.content-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    gap: 1rem;
+}
+
+.text-content {
+    flex: 1;
+}
+
+.video-wrapper {
+    flex: 0 0 33.333%;
+    /* Takes up 1/3 of the container width */
+    max-width: 33.333%;
+}
+
+.video-container {
+    position: relative;
+    width: 100%;
+    padding-top: 56.25%;
+    /* 16:9 Aspect Ratio */
+}
+
+.video-container iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+@media (max-width: 768px) {
+    .content-wrapper {
+        flex-direction: column;
+    }
+
+    .video-container {
+        max-width: 100%;
+    }
 }
 </style>
