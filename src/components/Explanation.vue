88cece8b6f1f292cd41358a2d8e3de7df0dfa46c
Ed <ed.jones@gmail.com>
2024-09-20 15:59:50 -0400

    Create a component for the caution message

--- src/components/Explanation.vue
+++ src/components/Explanation.vue
@@ -18,13 +18,7 @@
             <div v-if="quizItem.citations && quizItem.citations.length > 0" class="citations-wrapper">
                 <Citation v-for="(citation, index) in quizItem.citations" :key="index" :citation="citation" />
             </div>
-            <div v-if="quizItem.caution != ''" class="flex flex-row mt-8">
-                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
-                    <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc. -->
-                    <path opacity="1" fill="#FFBF00"
-                        d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" />
-                </svg> <span class="lg:pl-5 caution-message">{{ quizItem.caution }}</span>
-            </div>
+            <Caution :message="quizItem.caution" />
         </div>
     </div>
 </template>
@@ -32,12 +26,14 @@
 <script>
 import ResponsiveVideo from './ResponsiveVideo.vue'
 import Citation from './Citation.vue'
+import Caution from './Caution.vue'
 
 export default {
     name: 'Explanation',
     components: {
         ResponsiveVideo,
-        Citation
+        Citation,
+        Caution
     },
     props: {
         quizItem: {
