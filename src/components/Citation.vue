01fd6d67be1961d1a9906497fd4fe42a7e197b0c
Ed <ed.jones@gmail.com>
2024-09-20 15:13:43 -0400

    Start to add citation data file and vue component.

---
+++ src/components/Citation.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="citation">
+    <p class="citation-title">{{ citation.title }}</p>
+    <p class="citation-author">{{ citation.author }}</p>
+    <p class="citation-year">{{ citation.year }}</p>
+    <a :href="citation.url" target="_blank" class="citation-link">Read more</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Citation',
+  props: {
+    citation: {
+      type: Object,
+      required: true,
+      default: () => ({
+        title: '',
+        author: '',
+        year: '',
+        url: ''
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.citation {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  border-radius: 4px;
+  margin: 1rem 0;
+}
+
+.citation-title {
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+
+.citation-author,
+.citation-year {
+  font-size: 1rem;
+  color: #555;
+}
+
+.citation-link {
+  color: #42b983;
+  text-decoration: none;
+}
+
+.citation-link:hover {
+  text-decoration: underline;
+}
+</style>
--- src/components/Explanation.vue
+++ src/components/Explanation.vue
@@ -15,11 +15,13 @@
                 </div>
                 <ResponsiveVideo v-if="quizItem.videoId" :videoId="quizItem.videoId" />
             </div>
+            <Citation v-if="quizItem.citation" :citation="quizItem.citation" />
             <!----
       <p><a
           href="https://www.researchgate.net/profile/Matthew-Burns-10/publication/321116132_Meta-analysis_of_targeted_small-group_reading_interventions/links/5ab162340f7e9b4897c39acd/Meta-analysis-of-targeted-small-group-reading-interventions.pdf">{{
             quizItem.citation.title }}</a></p>
       -->
+
             <div v-if="quizItem.caution != ''" class="flex flex-row mt-8">
                 <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
                     <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc. -->
@@ -33,11 +35,13 @@
 
 <script>
 import ResponsiveVideo from './ResponsiveVideo.vue'
+import Citation from './Citation.vue'
 
 export default {
     name: 'Explanation',
     components: {
-        ResponsiveVideo
+        ResponsiveVideo,
+        Citation
     },
     props: {
         quizItem: {
