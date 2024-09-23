caa17055e11c944996f456b00d4b47cc28ecfb11
Ed <ed.jones@gmail.com>
2024-09-23 12:47:55 -0400

    Make the Next/Check button blank if nothing selected

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -34,6 +34,9 @@
   <div v-else-if="basicMode && chosen">
     <button class="bg-stone-400 w-32 h-10 mt-6 text-amber-400" @click="checkIt">Check it</button>
   </div>
+  <div v-else-if="basicMode && !reviewMode && !chosen">
+    <button class="bg-stone-400 w-32 h-10 mt-6 text-amber-400" @click="checkIt">&nbsp;</button>
+  </div>
   <div v-else>
     <button class="bg-stone-400 w-32 h-10 mt-6 text-amber-400" @click="nextItem">Next</button>
 
