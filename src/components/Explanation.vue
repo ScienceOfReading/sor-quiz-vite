851cc4ced35c6c4ae81f7ba85e0e04624d380b97
Ed <ed.jones@gmail.com>
2024-09-25 05:51:30 -0400

    Cursor tries to back up, but misses the mark.

--- src/components/Explanation.vue
+++ src/components/Explanation.vue
@@ -1,21 +1,19 @@
 <template>
-    <div class="explanation">
+    <div v-if="showExplanation" class="explanation">
         <div class="content-wrapper">
             <div class="text-content justified">
-                <!-- Your existing text content here -->
-                <p class=" mb-4">{{ quizItem.explanation }}</p>
-                <p class="">{{ quizItem.explanation2 }}</p>
-                <p class="0">{{ quizItem.explanation3 }}</p>
-                <p v-if="quizItem.ref1 != ''" class="mt-2 text-sm">{{ quizItem.ref1 }}</p>
-                <p v-if="quizItem.ref2 != ''" class="text-sm">{{ quizItem.ref2 }}</p>
-                <p v-if="quizItem.ref3 != ''" class="mb- text-sm">{{ quizItem.ref3 }}</p>
+                <p v-if="quizItem.explanation">{{ quizItem.explanation }}</p>
+                <p v-if="quizItem.explanation2">{{ quizItem.explanation2 }}</p>
+                <p v-if="quizItem.explanation3">{{ quizItem.explanation3 }}</p>
+                <p v-if="quizItem.closingText">{{ quizItem.closingText }}</p>
+                <p v-if="quizItem.closingText2">{{ quizItem.closingText2 }}</p>
             </div>
             <ExplainerVideo v-if="quizItem.videoId" :videoId="quizItem.videoId" :caption="quizItem.videoCaption"
                 :startTime="quizItem.videoStartTime" />
             <ExplainerImage v-else-if="quizItem.imageUrl" :imageUrl="quizItem.imageUrl"
                 :altText="quizItem.imageAltText" />
         </div>
-        <div class="citations-wrapper">
+        <div v-if="quizItem.citations && quizItem.citations.length" class="citations-wrapper">
             <Citation v-for="(citation, index) in quizItem.citations" :key="index" :citation="citation" />
         </div>
     </div>
@@ -37,6 +35,14 @@ export default {
         quizItem: {
             type: Object,
             required: true
+        },
+        userAnswer: {
+            type: [String, Number],
+            required: true
+        },
+        showExplanation: {
+            type: Boolean,
+            required: true
         }
     }
 }
@@ -44,24 +50,31 @@ export default {
 
 <style scoped>
 .explanation {
-    /* Add any necessary styles */
+    margin-bottom: 2rem;
 }
 
 .content-wrapper {
     display: flex;
     flex-wrap: wrap;
     gap: 1rem;
+    margin-bottom: 1rem;
 }
 
 .text-content {
     flex: 1 1 100%;
+    font-size: 1rem;
+    line-height: 1.6;
+    color: #333;
+}
+
+.justified {
+    text-align: justify;
 }
 
 .citations-wrapper {
     flex: 0 0 66.666%;
     max-width: 66.666%;
     margin-left: auto;
-    /* Align to the right */
     margin-top: 1rem;
 }
 
@@ -75,6 +88,11 @@ export default {
         flex: 0 0 100%;
         max-width: 100%;
         margin-left: 0;
+    }
+
+    .text-content {
+        font-size: 0.9rem;
+        line-height: 1.5;
     }
 }
 </style>
