502c9ba6a4e9693d05db5b6db2e68e9428b342ec
Ed <ed.jones@gmail.com>
2025-01-18 15:16:24 -0500

    Start to develop Legislation Policy quiz set

--- src/data/quizSets.js
+++ src/data/quizSets.js
@@ -40,5 +40,26 @@ export const quizSets = [{
         description: "'The science of reading tells us WHAT to teach... The science of learning tells us HOW to teach...' - Anna Geiger",
         podcastStartTime: 0,
     }
+},
+{
+    setName: "Legislation and Policy",
+    basicMode: true,
+    items: [40],
+    inProgress: true,
+    resource: {
+        title: "Science of Reading (SoR) Legislation and Implementation State Scan ",
+        url: "https://learning.ccsso.org/science-of-reading-legislation-and-implementation-state-scan",
+        description: "This guide provides a comprehensive overview of the science of reading, including its key principles, research findings, and implications for education policy and practice.",
+        resourceType: "PDF",
+        resourceUrl: "https://www.education.gov.au/sites/default/files/documents/2024/09/19/science-of-reading-guide-for-educators.pdf",
+    },
+    podcastEpisode: {
+        title: "Why the science of reading needs the science of learning",
+        EpisodeUrl: "https://lnns.co/njyfzcC9hg0",
+        audioUrl: "/audio/geiger.mp3",
+        description: "'The science of reading tells us WHAT to teach... The science of learning tells us HOW to teach...' - Anna Geiger",
+        podcastStartTime: 0,
+    }
 }
+
 ];
